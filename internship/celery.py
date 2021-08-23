651f8e0d17b20f1b08cf21e4eac59d02f5b2e2ee
vatsal70 <68066033+vatsal70@users.noreply.github.com>
2021-08-23 19:30:04 +0530

    Update celery.py

--- internship/celery.py
+++ internship/celery.py
@@ -5,23 +5,33 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'internship.settings')
 
 app = Celery('internship')
+app.conf.enable_utc = False
 
+app.conf.update(timezone = 'Asia/Kolkata')
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# app.conf.beat_schedule = {
-#     "every day between 6 AM & 18 PM": {
-#         "task": "databases.task.something",  # <---- Name of task
-#         "schedule": crontab()
-#     },
+# Celery Beat Settings
+app.conf.beat_schedule = {
+    # Executes every hour.
+    'collect-news-every-hour': { 
+         'task': 'databases.task.demo_program', 
+         'schedule':  60.0,
+        },          
+}
+
+# CELERY_BEAT_SCHEDULE = {
+#     # Executes every hour.
+#     'collect-news-every-hour': { 
+#          'task': 'databases.task.demo_program', 
+#          'schedule':  60.0,
+#         },          
 # }
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
-
-
 @app.task(bind=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
