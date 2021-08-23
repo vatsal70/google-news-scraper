be9973480edfae63e709320ff28521c90bd136aa
vatsal70 <68066033+vatsal70@users.noreply.github.com>
2021-08-23 16:41:56 +0530

    Update views.py

--- internship/views.py
+++ internship/views.py
@@ -22,50 +22,8 @@ headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Geck
                }
 
 def homepage(request):
-    # r = requests.get(f'https://news.google.com/topstories?hl=en-IN&gl=IN&ceid=IN:en', headers=headers)
-    # print("yes")
-    # data = r.text
-    # soup = BeautifulSoup(data, features="html.parser")
-    
-    # demo_program.delay()
     news = News.objects.all()
-    # print(news)
-    # # Get the url request to extract the number of results for the query.
-    # r = requests.get(f'https://news.google.com/topstories?hl=en-IN&gl=IN&ceid=IN:en', headers=headers)
-    # data = r.text
-    # soup = BeautifulSoup(data, features="html.parser")
-    # # soup=BeautifulSoup(data,'html5lib')
-    
-    
-    # for title in soup.find_all('div', class_ = "xrnccd"):
-    #     for item in title.find('article', class_ = "MQsxIb"):
-    #         text_a = item.find('h3')
-    #         text_link = item.find('a', class_ = 'RZIKme')
-    #         text_title = title.find('h3').text
-    #         # image = title.find('a').find('figure').find('img').get('src')
-    #         image = title.find('a').find('figure')
-    #         if image == None:
-    #             pass
-    #         else:
-    #             image = image.find('img').get('src')
-    #             if text_link == None:
-    #                 pass
-    #             else:
-    #                 text_link = text_link.get('href')
-    #                 text_link= text_link.replace("./", "news.google.com/")
-    #                 if text_a==None and text_link==None:
-    #                     pass
-    #                 else:
-    #                     print("                                                                                           ")
-    #                     print("Start-----------------------------------------------------------------------------------------------------------------------------------------------")
-    #                     print("Text", text_title)
-    #                     print("URL", 'https://' +text_link)
-    #                     print("Image", image)
-    #                     print("End----------------------------------------------------------------------------------------------------")
-      
-        # something.delay()
     params = {
         'news': news,
         }
-    demo_program.delay()
     return render(request, 'internship/index.html', params)
